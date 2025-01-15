ebcacdcc0700865a7cda059ed814da71867e50b3
Lilian Schall <lilian.schall@epita.fr>
2025-01-15 19:01:59 +0100

    feat: added document embedding into elastic search

--- pageindexer/indexer/document_creator.py
+++ pageindexer/indexer/document_creator.py
@@ -1,19 +1,27 @@
 import shopify
+from sentence_transformers import SentenceTransformer
 
-def create_product_document(product: shopify.Product):
-    product_options_values = [ option.values for option in product.attributes["options"]]
+
+def create_product_document(model: SentenceTransformer, product: shopify.Product):
+    product_options_values = [option.values for option in product.attributes["options"]]
 
     product_options = sum(product_options_values, [])
 
-    product_options = [option for option in product_options 
-                       if option.lower() != "default title"]
+    product_options = [
+        option for option in product_options if option.lower() != "default title"
+    ]
+
+    title = product.attributes["title"].lower()
+    description = product.attributes["body_html"]
+    embedding = model.encode(title + " - " + description, show_progress_bar=False)
 
     document = {
-        "title": product.attributes["title"].lower(),
-        "description": product.attributes["body_html"],
+        "title": title,
+        "description": description,
         "vendor": product.attributes["vendor"].upper(),
         "image_link": product.attributes["image"].src,
-        "options": product_options
+        "options": product_options,
+        "vector": embedding,
     }
 
     print("Indexer - Created following document:")
--- pageindexer/indexer/indexer.py
+++ pageindexer/indexer/indexer.py
@@ -2,6 +2,7 @@ import queue
 
 import shopify
 import elasticsearch
+from sentence_transformers import SentenceTransformer
 
 from ..env import ES_HOST, ES_PORT, ES_API_KEY
 from .document_creator import create_product_document
@@ -11,9 +12,10 @@ def __setup_elasticsearch():
     return elasticsearch.Elasticsearch(address, api_key=ES_API_KEY)
 
 def __index_product(es_client: elasticsearch.Elasticsearch,
+                    model: SentenceTransformer,
                     index: str, product: shopify.Product):
     product_id = product.attributes["id"]
-    product_document = create_product_document(product)
+    product_document = create_product_document(model, product)
     
     es_client.index(
         index=index,
@@ -22,19 +24,32 @@ def __index_product(es_client: elasticsearch.Elasticsearch,
     pass
 
 def __process_idexing(es_client: elasticsearch.Elasticsearch,
+                      model: SentenceTransformer,
                       access_token: str, shop_name: str):
     print("Indexer - Activating shopify session with token...")
-    session = shopify.Session(shop_name, "2023-10", access_token)
+    session = shopify.Session(shop_name, "2025-01", access_token)
 
     shopify.ShopifyResource.activate_session(session)
     print("Indexer - Session activated !")
     
     products = shopify.Product._find_every()
     try:
-        es_client.indices.create(index=shop_name)
+        es_index = {
+            "mappings": {
+                "properties": {
+                    "title": {"type": "text"},
+                    "description": {"type": "text"},
+                    "vendor": {"type": "keyword"},
+                    "image_link": {"type": "text"},
+                    "options": {"type": "keyword"},
+                    "vector": {"type": "dense_vector", "dims": 768, "index": True, "similarity": "cosine"}
+                }
+            }
+        }
+        es_client.indices.create(index=shop_name, body=es_index)
         print(f"Indexer - Created index {shop_name} in Elastic !")
         for product in products:
-            __index_product(es_client, shop_name, product)
+            __index_product(es_client, model, shop_name, product)
     except Exception as exception:
         print(f"An error occurred while indexing {shop_name}:")
         print(exception)
@@ -43,13 +58,15 @@ def __process_idexing(es_client: elasticsearch.Elasticsearch,
 
 
 def index_shop(process_queue: queue.Queue):
-    es_client = __setup_elasticsearch()
+    es_client: elasticsearch.Elasticsearch = __setup_elasticsearch()
+    model: SentenceTransformer = SentenceTransformer("quora-distilbert-multilingual")
     print("Indexer - Initialized ES client")
+
 
     while True:
         (access_token, shop_name) = process_queue.get(block=True)
         print("Indexer - Get access_token and shop_name !")
 
-        __process_idexing(es_client, access_token, shop_name)
+        __process_idexing(es_client, model, access_token, shop_name)
 
         process_queue.task_done()
